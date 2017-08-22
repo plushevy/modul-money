5071a4bb41064bd784febdb9899b2e348564de56
Molchanov Ilia <plue@inbox.ru>
2017-08-22 16:55:20 +0300

    близко к финалу

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,6 @@ gulp.task("serve", ["style"], function() {
     ui: false
   });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("sass/**/*.{scss,sass}",{readDelay: 100}, ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
